b111f30f33289ec563b8b3fdafe9cba5ca382a04
aprouco <adripenasrouco@gmail.com>
2022-01-19 14:12:25 +0100

    Array indexado

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h3>Definición de arrays en PHP</h3>
+
+    <?php
+        //Array indexado
+        $amigos=array("Antón","Rosalía","Xoán");
+        echo "O terceiro amigo é: " . $amigos[2]."<br/>";
+
+    
+        echo "O segundo coche de Xoán é " .$cochesAmigos['Xóan'][1] . "<br/>";
+    ?>
+</body>
+</html>
